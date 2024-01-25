8a6410082c2248d476089861eb2f2482fd977f2a
Koekoek1337 <hajo-groen@mail.com>
2024-01-25 10:52:56 +0100

    Show relative cells instead of absolute cells in second plot

--- model/classes/visualizer.py
+++ model/classes/visualizer.py
@@ -8,6 +8,8 @@ from typing import List
 class Visualizer:
     def __init__(self, model: CancerImmuneModel) -> None:
         self.model = model
+        self.latticeCells = self.model.dim[0] * self.model.dim[1]
+
         fig, axs = plt.subplots(1, 2)
         ax0 = axs[0]
         ax1 = axs[1]
@@ -27,8 +29,8 @@ class Visualizer:
             self.ax0.clear()
             self.ax0.imshow(self.model.cancerLattice * np.invert(self.model.immuneLattice.astype(bool)) + self.model.immuneLattice) 
 
-            self.immuneCells.append(self.model.get_nImmuneCells())
-            self.cancerCells.append(self.model.get_nCancerCells())
+            self.immuneCells.append(self.model.get_nImmuneCells() / self.latticeCells)
+            self.cancerCells.append(self.model.get_nCancerCells() / self.latticeCells)
             self.ax1.clear()
             self.ax1.plot(self.immuneCells, label="Immune")
             self.ax1.plot(self.cancerCells, label="Cancer")
