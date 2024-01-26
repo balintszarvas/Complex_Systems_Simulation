1430acf37beb97a943baa1abb7893c3e57f1ab7f
Hajo <hajo-groen@mail.com>
2024-01-26 14:52:51 +0100

    main testing

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
 from model.classes.visualizer import Visualizer
 from model.classes.cancerImmuneModel import CancerImmuneModel
 
+LEN         = 200
+FRAC_IMMUNE = 0.0060
 
 def main():
     """Simple test function that runs the model in visualization mode with set parameters"""
-    model = CancerImmuneModel(200, 200, 1.0, 0.01)
+    model = CancerImmuneModel(LEN, LEN, 0.7, 0.01, 0.01)
     model.seedCancer(100)
     
     #for i in range(100):
     #    model.timestep()
     
-    model.seedImmune(round(200 * 200 * 0.020))
+    model.seedImmune(round(LEN**2 * FRAC_IMMUNE))
 
     vis = Visualizer(model)
     vis.run()
